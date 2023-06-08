7b3466abeb8a15ba6067f682531c6369a6a9e157
wangjinliang1991 <wjl31802@126.com>
2023-06-08 09:25:43 +0800

    html escape

--- 02.test.js
+++ 02.test.js
@@ -3,3 +3,11 @@ const larry = require('./larry-tools')
 // 格式化时间
 const dtStr = larry.dateFormat(new Date())
 console.log(dtStr);
+
+const htmlStr = '<h1 title="abc">这是h1标签<span>123&nbsp;</span></h1>'
+
+const str = larry.htmlEscape(htmlStr)
+console.log(str);
+
+const str2 = larry.htmlUnEscape(str)
+console.log(str2);
--- larry-tools/index.js
+++ larry-tools/index.js
@@ -20,5 +20,38 @@ function padZero(n) {
 }
 
 module.exports = {
-    dateFormat
+    dateFormat,
+    htmlEscape,
+    htmlUnEscape
+}
+
+// 定义转义HTML字符的函数
+function htmlEscape(htmlstr) {
+    return htmlstr.replace(/<|>|"|&/g, match =>{
+        switch (match) {
+            case '<':
+                return '&lt;'
+            case '>':
+                return '&gt;'
+            case '"':
+                return '&quot;'
+            case '&':
+                return '&amp;'
+        }
+    })
+}
+
+function htmlUnEscape(str) {
+    return str.replace(/&lt;|&gt;|&quot;|&amp;/g, match=>{
+        switch (match) {
+            case '&lt;':
+                return '<'
+            case '&gt;':
+                return '>'
+            case '&quot;':
+                return '"'
+            case '&amp;':
+                return '&'
+        }
+    })
 }
